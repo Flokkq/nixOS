cd549b7cb7ac7b30f34d83a699cef20385c2b158
Flokkq <webcla21@htl-kaindorf.at>
2024-07-16 01:19:27 +0200

    add system settings and homebrew

--- hosts/darwin/configuration.nix
+++ hosts/darwin/configuration.nix
@@ -3,37 +3,43 @@
 {
   imports = [
     inputs.home-manager.darwinModules.home-manager
+    ./system.nix
+    ./apps.nix
   ];
 
   nixpkgs.hostPlatform = "aarch64-darwin";
-
-  services.nix-daemon.enable = true;
-
-  # Bootloader.
-  networking.hostName = "MacBook-Pro-von-Clemens";
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "MacBook-Pro-von-Clemens";
+  networking.computerName = "MacBook-Pro-von-Clemens";
+  system.defaults.smb.NetBIOSName = "MacBook-Pro-von-Clemens";
+
+  nix.settings.trusted-users = ["clemensweber"];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Vienna";
+
+  fonts.packages = [
+    pkgs.nerdfonts
+  ];
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = [
+    pkgs.nerdfonts
+  ];
+
+  nix.package = pkgs.nix;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.clemensweber = {
     description = "clemensweber";
     home = lib.mkForce "/Users/clemensweber"; 
-    packages = with pkgs; [
-      eza
-      oh-my-zsh
-      fzf
-      ripgrep
-      cloc
-      docker
-      bat
-      gh
-      neofetch
-      jq
-      typioca
-    ];
   };
 
   home-manager = {
@@ -44,15 +50,15 @@
     users.clemensweber = import ./home.nix;
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  nix.gc = {
+    automatic = lib.mkDefault true;
+    options = lib.mkDefault "--delete-older-than 7d";
+  };
 
-  fonts.packages = [
-    pkgs.nerdfonts
-  ];
-
-  # List packages installed in system profile. To search, run:
-  environment.systemPackages = [
-    pkgs.nerdfonts
-  ];
+  # Disable auto-optimise-store because of this issue:
+  #   https://github.com/NixOS/nix/issues/7273
+  # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
+  nix.settings = {
+    auto-optimise-store = false;
+  };
 }
