20a63c278a3809383f02cadf79db21639dd7c9bf
Flokkq <webcla21@htl-kaindorf.at>
2024-07-20 02:51:02 +0200

    sync

--- home/dev/git.nix
+++ home/dev/git.nix
@@ -9,5 +9,9 @@
     enable = true;
     userName = "Flokkq";
     userEmail = "webcla21@htl-kaindorf.at";
+    aliases = {
+      open = "!sh -c 'open $(git config --get remote.origin.url | sed -e s/.git$//)'";
+      sync = "!git add . && git commit -m 'sync' && git push -u origin main";
+    };
   };
 }
