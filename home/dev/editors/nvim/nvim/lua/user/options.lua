6d128189fcb4d05984844fed055a5f148fe63ff1
Flokkq <webcla21@htl-kaindorf.at>
2024-07-22 21:16:14 +0200

    add zelij

--- home/dev/editors/nvim/nvim/lua/user/options.lua
+++ home/dev/editors/nvim/nvim/lua/user/options.lua
@@ -76,7 +76,7 @@ vim.opt.colorcolumn = "80"
 
 vim.opt.guicursor = {
 	"n-v-c:block", -- Normal, visual, command-line: block cursor
-	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+	"i-ci-ve:block", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
 	"r-cr:hor20", -- Replace, command-line replace: horizontal bar cursor with 20% height
 	"o:hor50", -- Operator-pending: horizontal bar cursor with 50% height
 	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
