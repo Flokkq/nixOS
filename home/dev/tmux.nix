c576641c98ab44fe5363adc7a9c08aaf00ec6acc
Flokkq <webcla21@htl-kaindorf.at>
2024-08-19 03:06:18 +0200

    common(tmux): add pomodoro plugin

--- home/dev/tmux.nix
+++ home/dev/tmux.nix
@@ -9,8 +9,7 @@ let
       repo = "catppuccin-tmux";
       rev = "main";
       sha256 = "sha256-FJHM6LJkiAwxaLd5pnAoF3a7AE1ZqHWoCpUJE0ncCA8=";
-    };
-  };
+    }; };
   rose-pine = pkgs.tmuxPlugins.mkTmuxPlugin {
     pluginName = "rose-pine";
     version = "unstable-2023-01-06";
@@ -29,6 +28,16 @@ let
       repo = "tokyo-night-tmux";
       rev = "master";
       sha256 = "sha256-3rMYYzzSS2jaAMLjcQoKreE0oo4VWF9dZgDtABCUOtY=";
+    };
+  };  
+  tmuxPomodoroPlus = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-pomodoro-plus";
+    version = "main";
+    src = pkgs.fetchFromGitHub {
+      owner = "olimorris";
+      repo = "tmux-pomodoro-plus";
+      rev = "main";
+      sha256 = "sha256-LjhG2+DOAtLwBspOzoI2UDTgbUFWj7vvj6TQXqWw9z0="; 
     };
   };
 in
@@ -53,6 +62,7 @@ in
     tokyo-night
     catppuccin
     rose-pine
+    tmuxPomodoroPlus
 
     vim-tmux-navigator
     better-mouse-mode
@@ -77,6 +87,33 @@ in
   extraConfig = ''
     # catppuccin
     set -g @catppuccin_flavour 'mocha' # or frappe, macchiato, latte
+
+    # tmux-pomodoro-plus 
+    set -g status-left "#{pomodoro_status}"
+    set -g @pomodoro_granularity 'on'
+    set -g status-interval 1
+
+    set -g @pomodoro_toggle 'g'                    # Start/pause a Pomodoro/break
+    set -g @pomodoro_cancel 'G'                    # Cancel the current session
+    set -g @pomodoro_skip '_'                      # Skip a Pomodoro/break
+
+    set -g @pomodoro_mins 50                       # The duration of the Pomodoro
+    set -g @pomodoro_break_mins 5                  # The duration of the break after the Pomodoro completes
+    set -g @pomodoro_intervals 4                   # The number of intervals before a longer break is started
+    set -g @pomodoro_long_break_mins 25            # The duration of the long break
+    set -g @pomodoro_repeat 'off'                  # Automatically repeat the Pomodoros?
+    set -g @pomodoro_disable_breaks 'off'          # Turn off breaks
+
+    set -g @pomodoro_on "  "                      # The formatted output when the Pomodoro is running
+    set -g @pomodoro_complete " ✔︎"                 # The formatted output when the break is running
+    set -g @pomodoro_pause " ⏸︎"                    # The formatted output when the Pomodoro/break is paused
+    set -g @pomodoro_prompt_break " ⏲︎ break?"      # The formatted output when waiting to start a break
+    set -g @pomodoro_prompt_pomodoro " ⏱︎ start?"   # The formatted output when waiting to start a Pomodoro
+
+    set -g @pomodoro_menu_position "R"             # The location of the menu relative to the screen
+    set -g @pomodoro_sound 'off'                   # Sound for desktop notifications (Run `ls /System/Library/Sounds` for a list of sounds to use on Mac)
+    set -g @pomodoro_notifications 'off'           # Enable desktop notifications from your terminal
+    set -g @pomodoro_granularity 'off'             # Enables MM:SS (ex: 00:10) format instead of the default (ex: 1m)
 
     # set-default colorset-option -ga terminal-overrides ",xterm-256color:Tc"
     set -as terminal-features ",xterm-256color:RGB"
