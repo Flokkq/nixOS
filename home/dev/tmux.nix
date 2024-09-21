eed922682e9c83fd72cbe4d8d9737d4f395e578f
Flokkq <webcla21@htl-kaindorf.at>
2024-09-21 10:42:43 +0200

    common(zathura): add keybind to open files in zathura

--- home/dev/tmux.nix
+++ home/dev/tmux.nix
@@ -157,6 +157,7 @@ in
     bind-key -T copy-mode-vi v send-keys -X begin-selection
     bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
     bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+    bind-key -r a run-shell "tmux new-window 'nohup zathura $(fzf)'"
 
     bind '"' split-window -v -c "#{pane_current_path}"
     bind % split-window -h -c "#{pane_current_path}"
