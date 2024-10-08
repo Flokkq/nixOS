62855b93594572f728347810d6d137cac3828df2
Flokkq <webcla21@htl-kaindorf.at>
2024-10-08 21:52:34 +0200

    linux(base): init gpg

--- hosts/linux/configuration.nix
+++ hosts/linux/configuration.nix
@@ -61,6 +61,14 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
   # Enable GVfs
   services.gvfs.enable = true;
 
