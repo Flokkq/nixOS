047bad9d9a707cd0142fe68bae62c57966e3b693
Flokkq <webcla21@htl-kaindorf.at>
2024-10-08 21:23:55 +0200

    common(modules): add unfree packages

---
+++ modules/unfree.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) (
+      map lib.getName [
+        pkgs.discord
+        pkgs.unstable.keymapp
+        pkgs.steam
+        pkgs.steam-run
+        pkgs.steam-original
+        pkgs.obsidian
+        pkgs.slack
+      ]
+    );
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    discord
+    unstable.keymapp
+    obsidian
+    signal-desktop
+    slack
+  ];
+}
