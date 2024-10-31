0453c9dbdf49363abb701e8424738ff40033ff4d
Flokkq <webcla21@htl-kaindorf.at>
2024-10-31 18:23:13 +0100

    linux(hyprpaper): set wallpaper for all monitors correctly

--- home/linux/hypr/hyprpaper.nix
+++ home/linux/hypr/hyprpaper.nix
@@ -5,6 +5,11 @@
   ...
 }: let
   monitorUtils = import ./lib/utils.nix {inherit lib;};
+
+  # Automatically set wallpaper for each monitor
+  wallpaperConfig = lib.concatStringsSep "\n" (map (monitor:
+    "wallpaper = ${monitor.name},~/nixos-config/home/pictures/_1.png"
+  ) meta.monitors);
 in {
   home.packages = with pkgs; [
     hyprpaper
@@ -12,5 +17,6 @@ in {
 
   home.file.".config/hypr/hyprpaper.conf".text = ''
     preload = ~/nixos-config/home/pictures/_1.png
+    ${wallpaperConfig}
   '';
 }
