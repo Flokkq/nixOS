6a1fd30b61fccd4473b3a7f0899da8fabff96ad4
Flokkq <webcla21@htl-kaindorf.at>
2024-11-03 12:55:27 +0100

    darwin(dock): add basic dock config

--- hosts/darwin/cornflower-scientist/system.nix
+++ hosts/darwin/cornflower-scientist/system.nix
@@ -24,7 +24,24 @@
       # customize dock
       dock = {
         autohide = true;
+        mru-spaces = false; # disable rearragne spaces based on most recent use
         show-recents = false; # disable recent apps
+        static-only = false; # disable open apps
+        showhidden = true;
+
+
+        magnification = true;
+        largesize = 76;
+        tilesize = 48;
+
+        persistent-apps = [
+          "/Applications/Safari.app"
+          "/System/Applications/Utilities/Terminal.app"
+        ];
+
+        persistent-others = [
+          "~/Downloads"
+        ];
 
         # customize Hot Corners
         wvous-tl-corner = 2; # top-left - Mission Control
@@ -35,12 +52,14 @@
 
       # customize finder
       finder = {
+        FXPreferredViewStyle = "clmv"; # columnview in finder 
         _FXShowPosixPathInTitle = true; # show full path in finder title
         AppleShowAllExtensions = true; # show all file extensions
         FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
         QuitMenuItem = true; # enable quit menu item
         ShowPathbar = true; # show path bar
         ShowStatusBar = true; # show status bar
+        CreateDesktop = false;
       };
 
       # customize trackpad
