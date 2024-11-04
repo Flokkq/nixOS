6889fa07fc9c1413c32e26ec7a3632d4edf54656
Flokkq <webcla21@htl-kaindorf.at>
2024-11-04 15:36:35 +0100

    darwin(vesktop): add vesktop config

---
+++ home/darwin/apps/communication/vesktop.nix
@@ -0,0 +1,3 @@
+{pkgs, ...}: {
+  home.file."Library/Application\ Support/vesktop/themes/style.css".text = ''@import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-pink.theme.css")'';
+}
