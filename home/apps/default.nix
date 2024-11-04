de0599b93c665180ac718b5d7133830505718f93
Flokkq <webcla21@htl-kaindorf.at>
2024-11-04 15:37:08 +0100

    common(spicetify): move spicetify config to common

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -1,7 +1,14 @@
 {pkgs, ...}: {
   imports = [
     ./browsers
+    ./music
 
     ./zathura.nix
+  ];
+
+  home.packages = with pkgs; [
+    wireshark
+    postman
+    jetbrains.idea-ultimate
   ];
 }
--- home/linux/apps/default.nix
+++ home/linux/apps/default.nix
@@ -1,7 +1,6 @@
 {...}: {
   imports = [
     ./communication
-    ./music
     ./browsers
   ];
 
