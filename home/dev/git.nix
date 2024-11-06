2da9cb7512109ceb5d51fdb1b28e343dbd571a8c
Flokkq <webcla21@htl-kaindorf.at>
2024-11-06 22:03:18 +0100

    common(git): add signin key and signature

--- home/dev/git.nix
+++ home/dev/git.nix
@@ -7,8 +7,21 @@
     enable = true;
     userName = "Flokkq";
     userEmail = "webcla21@htl-kaindorf.at";
-
+    signing.key = "0xC8A75B77C0DB31B9";
+    signing.signByDefault = true;
+    ignores = [ "*~" ".DS_Store" ".direnv" ];
     extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+      # url = {
+      #   "git@github.com:" = {
+      #     insteadOf = "https://github.com/";
+      #   };
+      # };
+      pull = {
+        rebase = true;
+      };
       credential.helper = "!gh auth git-credential";
     };
   };
