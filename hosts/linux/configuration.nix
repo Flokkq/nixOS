6023275abfc85f5a3d3bb32b7c83b2c044ab1aa5
Flokkq <webcla21@htl-kaindorf.at>
2024-11-30 15:42:21 +0100

    linux(locale): extract locale settings to own file
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- hosts/linux/configuration.nix
+++ hosts/linux/configuration.nix
@@ -17,25 +17,7 @@
     ../../nixos/linux
   ];
 
-
   nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  time.timeZone = meta.timeZone or "Europe/Vienna";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_AT.UTF-8";
-    LC_IDENTIFICATION = "de_AT.UTF-8";
-    LC_MEASUREMENT = "de_AT.UTF-8";
-    LC_MONETARY = "de_AT.UTF-8";
-    LC_NAME = "de_AT.UTF-8";
-    LC_NUMERIC = "de_AT.UTF-8";
-    LC_PAPER = "de_AT.UTF-8";
-    LC_TELEPHONE = "de_AT.UTF-8";
-    LC_TIME = "de_AT.UTF-8";
-  };
 
   # Virtualisation
   virtualisation.docker.enable = true;
