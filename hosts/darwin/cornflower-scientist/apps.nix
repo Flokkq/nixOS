4e7083c15915dc09e51879811666f56f08610367
Flokkq <webcla21@htl-kaindorf.at>
2024-12-01 13:13:10 +0100

    darwin(apps): add missing gui-apps
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- hosts/darwin/cornflower-scientist/apps.nix
+++ hosts/darwin/cornflower-scientist/apps.nix
@@ -54,7 +54,7 @@
       rm -rf /Applications/Nix\ Apps
       mkdir -p /Applications/Nix\ Apps
       find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-      while read src; do
+      while read -r src; do
         app_name=$(basename "$src")
         echo "copying $src" >&2
         ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
@@ -127,6 +127,10 @@
       "1password"
       "1password-cli"
       "safe-exam-browser"
+      "raspberry-pi-imager"
+      "chatgpt"
+      "homerow"
+      "burp-suite"
     ];
   };
 }
