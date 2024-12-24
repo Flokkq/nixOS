e9af25715d6b842fef8f04ccf110eb2c70041292
Flokkq <webcla21@htl-kaindorf.at>
2024-12-24 20:09:15 +0100

    common(sops): add secrets structure
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- secrets.nix
+++ secrets.nix
@@ -1,2 +1,5 @@
 {
+  cerulean-statistician = import ./hosts/linux/cerulean-statistician/secrets.nix;
+  heliotrope-secretary = import ./hosts/linux/heliotrope-secretary/secrets.nix;
+  cornflower-scientist = import ./hosts/darwin/cornflower-scientist/secrets.nix;
 }
