31a3218898ec8db0574a5dea028e4d4ac52888c5
Flokkq <webcla21@htl-kaindorf.at>
2024-12-24 18:06:42 +0100

    chore(shell): add deadnix
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@ pkgs.mkShell {
       pkgs.nushell
       pkgs.just
       pkgs.pre-commit
+      pkgs.deadnix
     ]
     ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
       pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
