8f79b701c6b92ef3a3daa7216c8fbf2b49a3fe30
Flokkq <webcla21@htl-kaindorf.at>
2025-01-02 01:11:50 +0100

    common(vesktop): add vesktop settings
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- home/darwin/apps/communication/vesktop.nix
+++ home/darwin/apps/communication/vesktop.nix
@@ -1,3 +1,4 @@
 {...}: {
   home.file."Library/Application\ Support/vesktop/themes/style.css".text = ''@import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-pink.theme.css")'';
+  home.file."Library/Application\ Support/vesktop/settings/settings.json".source = ../../../apps/communication/vesktop.json;
 }
--- home/linux/apps/communication/vesktop.nix
+++ home/linux/apps/communication/vesktop.nix
@@ -1,3 +1,4 @@
 {...}: {
   home.file.".config/vesktop/themes/style.css".text = ''@import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-pink.theme.css")'';
+  home.file.".config/vesktop/settings/settings.json".source = ../../../apps/communication/vesktop.json;
 }
