a4ef2c18c51b3ed0960c2af916ffc32c8597f382
Flokkq <webcla21@htl-kaindorf.at>
2025-01-11 00:09:28 +0100

    darwin(sketchybar): fix illegal stuff
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- home/darwin/sketchybar/sketchybar/helpers/init.lua
+++ home/darwin/sketchybar/sketchybar/helpers/init.lua
@@ -1,11 +1,5 @@
 -- This will backfire soon enough :/
 -- TODO:  https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/lua.section.md
-local lua_path = "/nix/store/f6ani7d3cq70fd3j0qygbh08ms600xdh-SketchyBarLua-main/share/lua/5.4"
-if lua_path then
-  package.cpath = package.cpath .. ";" .. lua_path .. "/?.so"
-else
-  error("SB_LUA_PATH environment variable is not set")
-end
+package.cpath = package.cpath .. ";/Users/" .. os.getenv("USER") .. "/.local/share/sketchybar_lua/?.so"
 
--- Ensure the module is loaded correctly
-sbar = require("sketchybar")
+os.execute("(cd helpers && make)")
