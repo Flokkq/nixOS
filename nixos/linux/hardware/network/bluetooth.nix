045785a65581eaf36319c8cae5156d0a47e54600
Flokkq <webcla21@htl-kaindorf.at>
2025-06-20 00:00:52 +0200

    linux(bluetooth): enable
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- nixos/linux/hardware/network/bluetooth.nix
+++ nixos/linux/hardware/network/bluetooth.nix
@@ -1,9 +1,23 @@
 # Bluetooth Configuration
 #- <https://wiki.nixos.org/wiki/Bluetooth>
-_: {
+{pkgs, ...}: {
+  services.blueman.enable = true;
+  environment.systemPackages = with pkgs; [blueman bluez bluez-tools];
+
   hardware.bluetooth = {
-    enable = false;
-    powerOnBoot = false;
+    enable = true;
+    powerOnBoot = true;
     hsphfpd.enable = false;
+    package = pkgs.bluez;
+
+    settings.Policy.AutoEnable = "true";
+    settings.General = {
+      Enable = "Source,Sink,Media,Socket";
+      Name = "Hello";
+      ControllerMode = "dual";
+      FastConnectable = "true";
+      Experimental = "true";
+      KernelExperimental = "true";
+    };
   };
 }
