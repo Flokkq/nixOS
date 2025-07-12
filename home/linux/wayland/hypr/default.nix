cced15d832306615c52f73734b1a03a1a1e2156c
Flokkq <webcla21@htl-kaindorf.at>
2025-07-12 12:34:02 +0200

    linux(hypr): remove hardcoded paths to executables
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- home/linux/wayland/hypr/default.nix
+++ home/linux/wayland/hypr/default.nix
@@ -1,6 +1,7 @@
 {
   meta,
   lib,
+  pkgs,
   ...
 }: let
   monitorUtils = import ./utils.nix {inherit lib;};
@@ -109,29 +110,31 @@ in {
 
       bind =
         [
-          "$mainMod, E, exec,  wofi --show drun"
-          "$mainMod SHIFT, E, exec,  wofi --show=run"
-          "$mainMod SHIFT, N, exec, swaync-client -rs && swaync-client -t"
-          "$mainMod, Q, exec, ghostty"
+          "$mainMod, E, exec,  ${lib.getExe pkgs.wofi} --show drun"
+          "$mainMod SHIFT, E, exec,  ${lib.getExe pkgs.wofi} --show=run"
+          "$mainMod SHIFT, N, exec, ${pkgs.swaynotificationcenter}/bin/swaync-client -rs && ${pkgs.swaynotificationcenter}/bin/swaync-client -t"
+          "$mainMod, Q, exec, ${lib.getExe pkgs.ghostty}"
           "$mainMod, C, killactive"
-          "$mainMod, B, exec, google-chrome-stable"
-          "$mainMod, D, exec, vesktop"
-          "$mainMod, F, exec, nautilus"
-          "$mainMod, S, exec, spotify"
-          "$mainMod, P, exec, 1password"
-          "$mainMod, G, exec, gimp"
+          "$mainMod, B, exec, ${lib.getExe pkgs.google-chrome}"
+          "$mainMod, D, exec, ${lib.getExe pkgs.vesktop}"
+          "$mainMod, F, exec, ${lib.getExe pkgs.nautilus}"
+          "$mainMod, S, exec, ${lib.getExe pkgs.spotify}"
+          "$mainMod, P, exec, ${lib.getExe pkgs._1password}"
+          "$mainMod, G, exec, ${lib.getExe pkgs.gimp}"
 
-          "$mainMod, V, exec, pactl set-sink-mute   @DEFAULT_SINK@ toggle"
-          "$mainMod, U, exec, pactl set-source-mute @DEFAULT_SOURCE@ toggle"
-          "$mainMod, A, exec, pavucontrol"
+          "$mainMod, V, exec, ${pkgs.pulseaudio}/bin/pactl set-sink-mute   @DEFAULT_SINK@ toggle"
+          "$mainMod, U, exec, ${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle"
+          "$mainMod, A, exec, ${lib.getExe pkgs.pavucontrol}"
 
-          "$mainMod, X, exec, grimblast save screen - | swappy -f -"
-          "$mainMod SHIFT, X, exec, grimblast save area - | swappy -f -"
+          "$mainMod, X, exec, ${lib.getExe pkgs.grimblast} save screen - | ${lib.getExe pkgs.swappy} -f -"
+          "$mainMod SHIFT, X, exec, ${lib.getExe pkgs.grimblast} save area - | ${lib.getExe pkgs.swappy} -f -"
 
-          "$mainMod SHIFT, B, exec, blueman-manager"
-          "$mainMod, W, exec, nm-connection-editor"
+          "$mainMod SHIFT, B, exec, ${pkgs.blueman}/bin/blueman-manager"
+          "$mainMod, W, exec, ${pkgs.networkmanagerapplet}/bin/nm-connection-editor"
 
-          "$mainMod SHIFT, C, exec, pidof hyprlock || hyprlock"
+          "$mainMod, Z, exec, ${lib.getExe pkgs.woomer}"
+
+          "$mainMod SHIFT, C, exec, pidof ${lib.getExe pkgs.hyprlock} || ${lib.getExe pkgs.hyprlock}"
 
           # Window Management
           "$mainMod, SHIFT P, pseudo"
