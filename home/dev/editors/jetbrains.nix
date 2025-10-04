5ba2d075345c30347bfd610072fa8e897c29dc3f
Flokkq <webcla21@htl-kaindorf.at>
2025-10-05 00:49:45 +0200

    common(jetbrains): remove dead code
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- home/dev/editors/jetbrains.nix
+++ home/dev/editors/jetbrains.nix
@@ -1,42 +1,4 @@
-{pkgs, ...}: let
-  downloadPlugin = {
-    name,
-    version,
-    url,
-    hash,
-  }:
-    pkgs.stdenv.mkDerivation {
-      inherit name version;
-      src = pkgs.fetchzip {
-        inherit url hash;
-      };
-      installPhase = ''
-        mkdir -p $out
-        cp -r . $out
-      '';
-    };
-
-  catppuccin = downloadPlugin {
-    name = "catppuccin";
-    version = "3.4.0";
-    url = "https://downloads.marketplace.jetbrains.com/files/18682/633467/Catppuccin_Theme-3.4.0.zip";
-    hash = "sha256-a26wxw2/pJHt3SwNl0lzD/koKP77EhWpfNe3vw2rBco=";
-  };
-in {
-  home.packages = with pkgs; [
-    (jetbrains.plugins.addPlugins jetbrains.datagrip [
-      catppuccin
-    ])
-
-    (jetbrains.plugins.addPlugins jetbrains.idea-ultimate [
-      catppuccin
-    ])
-
-    (jetbrains.plugins.addPlugins jetbrains.rider [
-      catppuccin
-    ])
-  ];
-
+{ ...}: {
   home.file.".ideavimrc".text = ''
     let mapleader = ' '
     nnoremap <leader> <Nop>
@@ -55,21 +17,5 @@ in {
 
     nnoremap H ^
     nnoremap L $
-  '';
-
-  home.file.".config/JetBrains/IntelliJIdea2024.1/options/ui.lnf.xml".text = ''
-    <application>
-      <component name="LafManager">
-        <laf class-name="Catppuccin Mocha" />
-      </component>
-    </application>
-  '';
-
-  home.file.".config/JetBrains/IntelliJIdea2024.1/options/editor.xml".text = ''
-    <application>
-      <component name="EditorColorsManager">
-        <global_color_scheme name="Catppuccin Mocha" />
-      </component>
-    </application>
   '';
 }
