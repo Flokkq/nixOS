ba0d9f61a4adea9af23337785ae8cc85404ba2b6
Flokkq <webcla21@htl-kaindorf.at>
2025-10-14 19:22:53 +0200

    common(obsidian): change plugin to official fork
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- home/dev/editors/nvim/nvim/lua/plugins/obsidian.lua
+++ home/dev/editors/nvim/nvim/lua/plugins/obsidian.lua
@@ -1,20 +1,28 @@
 return {
     {
-        "epwalsh/obsidian.nvim",
-        tag = "v3.9.0",
-        requires = {
-            -- Required.
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            require("obsidian").setup({
-                workspaces = {
-                    {
-                        name = "personal",
-                        path = "~/vaults/personal",
-                    },
+        "obsidian-nvim/obsidian.nvim",
+        version = "*",
+        ft = "markdown",
+        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+        -- event = {
+        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+        --   -- refer to `:h file-pattern` for more examples
+        --   "BufReadPre path/to/my-vault/*.md",
+        --   "BufNewFile path/to/my-vault/*.md",
+        -- },
+        ---@module 'obsidian'
+        ---@type obsidian.config
+        opts = {
+            workspaces = {
+                {
+                    name = "personal",
+                    path = "~/vaults/personal",
                 },
-            })
-        end,
+            },
+            templates = {
+                folder = "~/vaults/personal/templates",
+            }
+        },
     }
 }
